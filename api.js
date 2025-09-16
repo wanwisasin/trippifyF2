eed5a13180b61ca15177d7bc24869e2d90df6859
wanwisasin <Wanwisa_j@cmu.ac.th>
2025-09-17 05:46:54 +0700

    ..

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://trippifyb2-production.up.railway.app";
 
 export default axios.create({
   baseURL: API_BASE_URL,
